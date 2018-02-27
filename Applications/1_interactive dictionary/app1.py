26e3620fddd5a06b0a10a70b52590b0568ec2bf8
Anh Pham <anh65498@gmail.com>
2018-02-26 20:49:57 -0800

    Dictionary project: loop program, format display (list -> string)

--- Applications/1_interactive dictionary/app1.py
+++ Applications/1_interactive dictionary/app1.py
@@ -2,7 +2,8 @@
 #How? Use JSON standard library
 #Learn? SequenceMatcher module, a module to find
 import json
-from difflib import SequenceMatcher        #module to compare text, need for sequencematcher
+from difflib import SequenceMatcher        #module to compare text and find close matches
+from difflib import get_close_matches
 dictionary = json.load(open("data.json", "r"))          #since load(fp) where fp is a file-like object, use "open()" to create it
 
 #receive: word(string) to look up
@@ -10,11 +11,45 @@ dictionary = json.load(open("data.json", "r"))          #since load(fp) where fp
 def get_definition(word):
     if word.lower() in dictionary:
         return dictionary[word]
+    elif len(get_close_matches(word,dictionary.keys())) > 0:
+        suggestion = get_close_matches(word, dictionary.keys())[0]
+        decision = input("Can't find %s. Did you mean %s. Y/N? " %(word, suggestion))
+        #dictionary.keys( ) returns a list of all keys in dictionary
+        if decision.lower() == "y" or decision.lower() == "yes":        #decision.lower() == "y" or "yes" does not work :( s
+            return dictionary[suggestion]
+        elif decision.lower() == "n" or decision.lower() == "no":
+            return("The word doesn't exist. Please double check it.")
+        else:
+            return "I do not understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
 
-#get word to look up from user
-word = input("Enter word you want to look up: ")
 
-#display definition to command line
-print(get_definition(word))
+def main():
+    #get a word to look up from user
+    word = input("Enter word you want to look up: ")
+
+    #get definition from get_definition()
+    definition = get_definition(word)
+
+    #display definition to screen
+    if (type(definition)) == list:          #print list of definition
+        for i in definition:
+            print("-", i)
+    else:                                   #print errors
+        print(definition)
+
+    return input("Do you want to look up another word? Y/N? ")
+
+redo = "yes"
+while (redo.lower() == "yes" or redo.lower() == "y"):
+    redo = main()
+
+
+#print(SequenceMatcher(None, "rain", "rainn").ratio( ))
+##ratio returns the similarity between the two strings  (0 -> 1)
+##SequenceMatcher's first argument is None or a function to ignore line breaks/whitespaces/markups ... between the two strings.
+##In our case, we don't have those "junk" data in our two strings,  so don't need a function. Hence, "None"
+
+#get_close_matches returns a returns a dict_key objects, but behaves like a list, of words similar to first argument from second arg
+#cuttoff is similarity's ratio
